90ebc4cd675334c075115615c58144b68802b04e
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-08 22:15:15 -0400

    Removed generated js files from source control; added to gitignore.

--- dev-resources/public/js/alpha_counter/core_test.js
+++
@@ -1,5 +0,0 @@
-// Compiled by ClojureScript 0.0-2234
-goog.provide('alpha_counter.core_test');
-goog.require('cljs.core');
-goog.require('cemerick.cljs.test');
-goog.require('cemerick.cljs.test');
--- dev-resources/public/js/brepl/connect.js
+++
@@ -1,4 +0,0 @@
-// Compiled by ClojureScript 0.0-2234
-goog.provide('brepl.connect');
-goog.require('cljs.core');
-goog.require('clojure.browser.repl');
