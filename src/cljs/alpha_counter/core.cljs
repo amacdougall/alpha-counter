60a3018a3ef98e7c42698cf9f4e3c27562de0ac1
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-08 22:21:02 -0400

    Correctly update player selected state in life-counter-view.

--- src/cljs/alpha_counter/core.cljs
+++ src/cljs/alpha_counter/core.cljs
@@ -26,7 +26,16 @@
                                  :history [])))
 
 (defn ready [app]
-  (om/transact! app #(assoc % :ready true)))
+  (om/transact! app :ready (constantly true)))
+
+(defn select-player [app player]
+  ; Since player is already a cursor, we need to set its value. Is this going
+  ; to be an ongoing pain point?
+  (om/transact! app #(assoc % :current-player (om/value player))))
+
+(defn is-current-player [app player]
+  (let [current-player (or (:current-player app) (first (:players app)))]
+    (= player current-player)))
 
 ; views
 (defn character-select-view [app owner]
@@ -53,8 +62,27 @@
                            :disabled (some empty? [p1 p2])}
             "Start!"))))))
 
-(defn life-counter-view [player owner]
-  (dom/h1 nil "Coming soon!"))
+(defn life-counter-view [app owner]
+  (let [p1 (-> app :players first)
+        p2 (-> app :players second)]
+    (dom/div nil
+      (dom/div #js {:className "players"}
+        ; TODO: a single character health component?
+        (dom/div (when (is-current-player app p1) #js {:className "selected"})
+          (dom/button #js {:onClick #(select-player app p1)}
+            (-> p1 :character :name))
+          (dom/div #js {:className "health-display"}
+            (dom/div #js {:className "damage"} "")
+            (dom/div #js {:className "bar"} "")
+            (dom/div #js {:className "number"} (:health p1))))
+        (dom/div (when (is-current-player app p2) #js {:className "selected"})
+          (dom/button #js {:onClick #(select-player app p2)}
+            (-> p2 :character :name))
+          (dom/div #js {:className "health-display"}
+            (dom/div #js {:className "damage"} "")
+            (dom/div #js {:className "bar"} "")
+            (dom/div #js {:className "number"} (:health p2))))))))
+
 
 (defmulti main-view (fn [app _] (:ready app)))
 
@@ -62,11 +90,11 @@
   (character-select-view app owner))
 
 (defmethod main-view true [app owner]
-  (life-counter-view (:current-player app) owner))
+  (life-counter-view app owner))
 
 (om/root main-view app-state
   {:target (. js/document (getElementById "main"))})
 
 ;; scratch below this line
 
-; (:players @app-state)
+; (:current-player @app-state)
