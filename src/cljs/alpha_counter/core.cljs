dd4aee29c267cfff62c499f2f981d73d58ee92b1
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-13 17:22:59 -0400

    Split player health/select display into a separate view.
    Also some inextricable refactoring around player selection: each player
    hash now has a :current key, select-player is improved, etc.

--- src/cljs/alpha_counter/core.cljs
+++ src/cljs/alpha_counter/core.cljs
@@ -16,26 +16,30 @@
 (def app-state
   (atom
     {:ready false ; when true, displays the main life counter
-     :current-player nil
      :players [{} {}]}))
 
 ; helpers
+;; Initializes the player by selecting the character. Sets player health
+;; to the character's max health, and empties player history.
 (defn select-character [player character]
   (om/transact! player #(assoc % :character character
+                                 :current false
                                  :health (:health character)
                                  :history [])))
 
 (defn ready [app]
   (om/transact! app :ready (constantly true)))
 
+;; Sets :current to true on the supplied player, false on all others. Can be
+;; used as a handler as (partial app player).
 (defn select-player [app player]
-  ; Since player is already a cursor, we need to set its value. Is this going
-  ; to be an ongoing pain point?
-  (om/transact! app #(assoc % :current-player (om/value player))))
+  (let [set-current #(assoc % :current (= % @player))]
+    (om/transact! app :players #(mapv set-current %))))
 
-(defn is-current-player [app player]
-  (let [current-player (or (:current-player app) (first (:players app)))]
-    (= player current-player)))
+;; Returns the current player, or player one.
+(defn get-current-player [app]
+  (or (first (filter :current (:players app)))
+      (first (:players app))))
 
 ; views
 (defn character-select-view [app owner]
@@ -47,11 +51,11 @@
       (let [p1 (-> app :players first)
             p2 (-> app :players second)
             icons (fn [player]
-                    (map (fn [c]
-                           (dom/button
-                             #js {:onClick #(select-character player c)}
-                             (:name c)))
-                         characters))]
+                    (mapv (fn [c]
+                            (dom/button
+                              #js {:onClick #(select-character player c)}
+                              (:name c)))
+                          characters))]
         (dom/div nil
           (dom/h1 nil "Character Select")
           (dom/h2 nil "Player One")
@@ -62,27 +66,40 @@
                            :disabled (some empty? [p1 p2])}
             "Start!"))))))
 
-(defn life-counter-view [app owner]
-  (let [p1 (-> app :players first)
-        p2 (-> app :players second)]
-    (dom/div nil
-      (dom/div #js {:className "players"}
-        ; TODO: a single character health component?
-        (dom/div (when (is-current-player app p1) #js {:className "selected"})
-          (dom/button #js {:onClick #(select-player app p1)}
-            (-> p1 :character :name))
+(defn health-view [props owner]
+  (reify
+    om/IRender
+    (render [_]
+      (let [{:keys [player select-player]} props]
+        (dom/div (when (:current player) #js {:className "selected"})
+          (dom/button #js {:onClick #(select-player player)}
+            (-> player :character :name))
           (dom/div #js {:className "health-display"}
             (dom/div #js {:className "damage"} "")
             (dom/div #js {:className "bar"} "")
-            (dom/div #js {:className "number"} (:health p1))))
-        (dom/div (when (is-current-player app p2) #js {:className "selected"})
-          (dom/button #js {:onClick #(select-player app p2)}
-            (-> p2 :character :name))
-          (dom/div #js {:className "health-display"}
-            (dom/div #js {:className "damage"} "")
-            (dom/div #js {:className "bar"} "")
-            (dom/div #js {:className "number"} (:health p2))))))))
+            (dom/div #js {:className "number"} (:health player))))))))
 
+
+(defn life-counter-view [app owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/div nil
+        ; player health bars
+        (apply dom/div #js {:className "players"}
+          (om/build-all health-view
+            (mapv (fn [p] ; build health-view arguments per player
+                    {:player p
+                     :select-player (partial select-player app p)})
+                  (:players app))))
+        ; combo damage buttons
+        (apply dom/div nil
+          (map (fn [n]
+                 ; TODO: feed these button clicks into a channel system instead
+                 (dom/button
+                   #js {:onClick #(.log js/console "life click: %d" n)}
+                   n))
+               (range 1 21)))))))
 
 (defmulti main-view (fn [app _] (:ready app)))
 
@@ -94,7 +111,3 @@
 
 (om/root main-view app-state
   {:target (. js/document (getElementById "main"))})
-
-;; scratch below this line
-
-; (:current-player @app-state)
