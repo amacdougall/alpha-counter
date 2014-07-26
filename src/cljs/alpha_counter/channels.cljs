03de3b3bc3c401bc7e8c83efc2947e6a578c4490
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-26 15:37:41 -0400

    Place tagged pairs in output chan of running-total.
    Instead of just emitting numbers, running-total will now emit pairs in
    the form `[:running-total 10]` or, after the timeout, `[:total 15]`.

--- src/cljs/alpha_counter/channels.cljs
+++ src/cljs/alpha_counter/channels.cljs
@@ -23,18 +23,19 @@
                   (recur (dec total))))))))
     out))
 
-;; Returns a channel which takes values from the input channel, and receives a
-;; running total. If the ms timeout expires without receiving another value,
-;; receives the special value :timeout.
+;; Returns a channel which takes values from the input channel, and receives
+;; either running total vecs in the form [:running-total n] or, if the timeout
+;; expires without an input value, a vec in the form [:total n].
 (defn running-total [in ms]
   (let [out (chan)]
+    ; TODO: only wait for timeout if (> total 0)
     (go (loop [total 0]
-          (let [[v ch] (alts! [in (timeout ms)])]
+          (let [[v ch] (alts! (if (zero? total) [in] [in (timeout ms)]))]
             (if (= ch in)
               (do
-                (>! out (+ total v))
+                (>! out [:running-total (+ total v)])
                 (recur (+ total v)))
               (do
-                (>! out :timeout)
+                (>! out [:total total])
                 (recur 0))))))
     out))
