c9e1ea84c303c40c686a31ee714e720502784c4d
Alan MacDougall <smoke@alanmacdougall.com>
2014-07-26 15:39:47 -0400

    Feed hit button clicks into combo channel.
    Combo channel output is just logged to the console right now, but it
    looks right!

--- src/cljs/alpha_counter/core.cljs
+++ src/cljs/alpha_counter/core.cljs
@@ -1,12 +1,8 @@
 (ns alpha-counter.core
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [chan >! <! alts! put! timeout close!]]
-            [alpha-counter.lib.async :as async]
-            [alpha-counter.channels :refer [trickle running-total]]
             [alpha-counter.views.character-select :refer [character-select-view]]
-            [alpha-counter.views.life-counter :refer [life-counter-view]])
-  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+            [alpha-counter.views.life-counter :refer [life-counter-view]]))
 
 (enable-console-print!)
 
@@ -15,30 +11,13 @@
     {:ready false ; when true, displays the main life counter
      :players [{} {}]}))
 
-
-(defmulti main-view (fn [app _] (:ready app)))
-
-(defmethod main-view false [app owner]
-  (character-select-view app owner))
-
-(defmethod main-view true [app owner]
-  (life-counter-view app owner))
+(defn main-view [app owner]
+  (dom/div nil
+    (if-not (:ready app)
+      (om/build character-select-view app)
+      (om/build life-counter-view app))))
+; NOTE: gotta om/build each thing individually if we want to run init-state; we
+; can't just call (desired-view app owner).
 
 (om/root main-view app-state
   {:target (. js/document (getElementById "main"))})
-
-; scratch
-
-; (def hp-in (chan))
-; (def hp (trickle hp-in 50))
-; (def combo (running-total hp 5000))
-; 
-; (def output-test
-;   (go (loop []
-;         (when-let [v (<! combo)]
-;           (do
-;             (.log js/console v)
-;             (recur))))))
-; 
-; (doseq [n [2 3 5 8 13]]
-;   (put! hp-in n))
--- src/cljs/alpha_counter/views/life_counter.cljs
+++ src/cljs/alpha_counter/views/life_counter.cljs
@@ -1,6 +1,12 @@
 (ns alpha-counter.views.life-counter
   (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
+            [om.dom :as dom :include-macros true]
+            [alpha-counter.channels :refer [running-total trickle]]
+            [cljs.core.async :refer [>! <! chan put!]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
+
+;; Time before combo is considered complete, in ms.
+(def combo-timeout 3000)
 
 ;; Sets :current to true on the supplied player, false on all others. Can be
 ;; used as a handler as (partial app player).
@@ -31,8 +37,32 @@
 
 (defn life-counter-view [app owner]
   (reify
-    om/IRender
-    (render [_]
+    om/IInitState
+    (init-state [_]
+      (.log js/console "lcv/init-state")
+      (let [hits (chan)
+            combo (running-total hits combo-timeout)]
+        {:hits hits, :combo combo}))
+    om/IWillMount
+    (will-mount [_]
+      (let [hits (om/get-state owner :hits)
+            combo (om/get-state owner :combo)]
+        ; display running total from combo channel on screen;
+        ; add grand total to opponent damage or self healing
+        (go-loop []
+          (let [v (<! combo)]
+            (.log js/console "combo loop got %o" v (pr-str v))
+            (if (= (first v) :running-total)
+              (.log js/console "running total: %d" (second v))
+              (.log js/console "grand total: %d" (second v))))
+          (recur))))
+    om/IRenderState
+    ; TODO: figure out why there is no state when this first runs; You'd think
+    ; that init-state would happen before render-state! That's just obvious.
+    ; ...but that's totally what's happening. I might not understand the
+    ; lifecycle as well as I thought. What's going on?
+    (render-state [this {:keys [hits combo] :as state}]
+      (.log js/console "lcv/render-state: state %o %s" state (pr-str state))
       (dom/div nil
         ; player health bars
         (apply dom/div #js {:className "players"}
@@ -44,8 +74,7 @@
         ; combo damage buttons
         (apply dom/div nil
           (map (fn [n]
-                 ; TODO: feed these button clicks into a channel system instead
                  (dom/button
-                   #js {:onClick #(.log js/console "life click: %d" n)}
+                   #js {:onClick #(put! hits n)}
                    n))
                (range 1 21)))))))
