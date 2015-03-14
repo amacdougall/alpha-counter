4b66ab79342eeafb3909da8ecd5801d8d6fd0ca4
Alan MacDougall <smoke@alanmacdougall.com>
2015-03-14 13:43:44 -0400

    Bump Figwheel to 0.2.5, ClojureScript to 3058.

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
             :url "http://opensource.org/licenses/MIT"}
 
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [org.clojure/clojurescript "0.0-2760" :scope "provided"]
-                 [figwheel "0.2.3-SNAPSHOT"]
+                 [org.clojure/clojurescript "0.0-3058" :scope "provided"]
+                 [figwheel "0.2.5"]
                  [org.omcljs/om "0.8.8"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
 
   :plugins [[lein-cljsbuild "1.0.4"]
-            [lein-figwheel "0.2.3-SNAPSHOT"]]
+            [lein-figwheel "0.2.5"]]
 
   :source-paths ["src"]
 
@@ -40,29 +40,6 @@
                          :closure-warnings {:externs-validation :off
                                             :non-standard-jsdoc :off}}}]}
 
-  :figwheel {
-             :http-server-root "public" ;; default and assumes "resources" 
+  :figwheel {:http-server-root "public" ;; default and assumes "resources"
              :server-port 3449 ;; default
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
-
-             ;; Server Ring Handler (optional)
-             ;; if you want to embed a ring handler into the figwheel http-kit
-             ;; server, this is simple ring servers, if this
-             ;; doesn't work for you just run your own server :)
-             ;; :ring-handler hello_world.server/handler
-
-             ;; To be able to open files in your editor from the heads up display
-             ;; you will need to put a script on your path.
-             ;; that script will have to take a file path and a line number
-             ;; ie. in  ~/bin/myfile-opener
-             ;; #! /bin/sh
-             ;; emacsclient -n +$2 $1
-             ;;
-             ;; :open-file-command "myfile-opener"
-
-             ;; if you want to disable the REPL
-             ;; :repl false
-
-             ;; to configure a different figwheel logfile path
-             ;; :server-logfile "tmp/logs/figwheel-logfile.log" 
-             })
+             :css-dirs ["resources/public/css"]}) ;; watch and update CSS
